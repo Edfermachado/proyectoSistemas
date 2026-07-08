8bb868a929833ece1a3e2a6f5f5921643a7561a9
Edfermachado <edfermachado10@gmail.com>
2026-07-07 21:22:36 -0400

    refactor: remove Payload CMS dependency and simplify project structure to standard Next.js setup

--- next.config.ts
+++ next.config.ts
@@ -1,34 +1,7 @@
-import { withPayload } from '@payloadcms/next/withPayload'
-import type { NextConfig } from 'next'
-import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(__filename)
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
-  output: 'standalone',
+  /* config options here */
+};
 
-  images: {
-    localPatterns: [
-      {
-        pathname: '/api/media/file/**',
-      },
-    ],
-  },
-  webpack: (webpackConfig) => {
-    webpackConfig.resolve.extensionAlias = {
-      '.cjs': ['.cts', '.cjs'],
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.mjs': ['.mts', '.mjs'],
-    }
-
-    return webpackConfig
-  },
-  turbopack: {
-    root: path.resolve(dirname),
-  },
-}
-
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default nextConfig;
